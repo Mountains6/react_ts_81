e3d20adfea0263023f29e0fe42c8b24bc920b111
YanaHeuer <YanaHeuer9@gmail.com>
2026-04-23 17:31:16 +0200

    Lesson14

--- src/App.tsx
+++ src/App.tsx
@@ -2,42 +2,24 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import GlobalStyles from "./styles/GlobalStyles";
 import Layout from "./components/Layout/Layout";
-
-// Импорты лекций
-// import Lesson07 from "./lessons/Lesson07/Lesson07";
-
-// import Lesson08 from "./lessons/Lesson08/Lesson08";
-// import Lesson07 from "./lessons/Lesson07/Lesson07";
-// import Lesson09 from "./lessons/Lesson09/Lesson09";
-// import Lesson10 from "./lessons/Lesson10/Lesson10";
-// import Lesson11 from "./lessons/Lesson11/Lesson11";
-import Home from "./pages/Home/Home";
-import About from "./pages/About/About";
-import Users from "./pages/Users/Users";
-import User from "./pages/Users/components/User/User";
+import { routesData } from "./routes/data";
+import { v4 } from "uuid";
+import type { RouteObj } from "./routes/types";
 
 function App() {
+  const routes = routesData.map((route: RouteObj) => {
+    return <Route key={v4()} path={route.path} element={route.element} />;
+  });
+
   return (
     <BrowserRouter>
       <GlobalStyles />
-      {/* Лекция 7. Map, TS */}
-      {/* <Lesson07/> */}
-      {/* Лекция 08/ */}
-      {/* <Lesson08 /> */}
-      {/* Лекция 9. Контроль компонентов */}
-      {/* <Lesson09 /> */}
-      {/* Лекция 10. ToDo List */}
-      {/* <Lesson10/> */}
-      {/* Лекция 11.  */}
-      {/* <Lesson11/> */}
       <Layout>
         {/* Routes - собирает все маршруты приложения */}
         <Routes>
           {/* Route - компонент, в которой передаётся маршрут и контент */}
-          <Route path="/" element={<Home></Home>} />
-          <Route path="/about" element={<About></About>} />
-          <Route path="/users" element={<Users></Users>} />
-          <Route path="/users/user" element={<User></User>} />
+          {/* <Route path="/" element={<Home></Home>} /> */}
+          {routes}
         </Routes>
       </Layout>
     </BrowserRouter>
---
+++ src/routes/data.tsx
@@ -0,0 +1,24 @@
+import type { RouteObj } from "./types";
+import Home from "../pages/Home/Home";
+import About from "../pages/About/About";
+import Users from "../pages/Users/Users";
+import User from "../pages/Users/components/User/User";
+import PageNotFound from "../pages/PageNotFound/PageNotFound";
+import Lesson07 from "../lessons/Lesson07/Lesson07";
+import Lesson08 from "../lessons/Lesson08/Lesson08";
+import Lesson09 from "../lessons/Lesson09/Lesson09";
+import Lesson10 from "../lessons/Lesson10/Lesson10";
+import Lesson11 from "../lessons/Lesson11/Lesson11";
+
+export const routesData: RouteObj[] = [
+  { path: "/", element: <Home /> },
+  { path: "/about", element: <About /> },
+  { path: "/users", element: <Users /> },
+  { path: "/users/user", element: <User /> },
+  { path: "/lesson07", element: <Lesson07 /> },
+  { path: "lesson08", element: <Lesson08 /> },
+  { path: "lesson09", element: <Lesson09 /> },
+  { path: "lesson10", element: <Lesson10 /> },
+  { path: "lesson11", element: <Lesson11 /> },
+  { path: "*", element: <PageNotFound /> },
+];
